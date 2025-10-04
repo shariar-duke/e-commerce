6a40bd3b686cd904eb5a6cfe3b217ef2d8e9d764
shariar-duke <shariarmahmud225@gmail.com>
2025-10-05 01:22:00 +0600

    fix : ready for prodution

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
 import HeroSlider from './components/HeroSlider'
 import TopProductsContainer from './components/TopProducts/TopProductsContainer'
-export default function Home() {
-  // const baseURL = process.env.NEXT_PUBLIC_BASE_URL
-  // const res = await fetch(`${baseURL}/api/products/top`, { cache: 'no-store' })
+export default async function Home() {
+  const baseURL = process.env.NEXT_PUBLIC_BASE_URL
+  const res = await fetch(`${baseURL}/api/products/top`, { cache: 'no-store' })
 
-  // const topProducts = await res.json()
-  // console.log('Products are', topProducts)
+  const topProducts = await res.json()
+  console.log('Products are', topProducts)
   return (
     <div className='w-[94vw] mx-auto'>
       <HeroSlider />
