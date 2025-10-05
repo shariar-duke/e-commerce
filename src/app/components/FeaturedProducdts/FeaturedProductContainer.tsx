9bc38c1e0d19512b04b4ed15be02a41725f34b13
shariar-duke <shariarmahmud225@gmail.com>
2025-10-05 20:13:32 +0600

    feat : animation problem fixed

--- src/app/components/FeaturedProducdts/FeaturedProductContainer.tsx
+++ src/app/components/FeaturedProducdts/FeaturedProductContainer.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import { Product } from '@/app/lib/type'
+import { useEffect, useState } from 'react'
 import 'swiper/css'
 import 'swiper/css/navigation'
 import 'swiper/css/pagination'
@@ -13,6 +14,28 @@ type FeaturedProductContainerProps = {
 }
 
 export default function FeaturedProductContainer({ products }: FeaturedProductContainerProps) {
+  const [isClient, setIsClient] = useState(false)
+
+  useEffect(() => {
+    setIsClient(true)
+  }, [])
+
+  if (!isClient) {
+    return (
+      <div className='mt-6'>
+        <h2 className='text-2xl font-bold mb-4'>Featured Products</h2>
+        <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4'>
+          {products.slice(0, 5).map((_, i) => (
+            <div
+              key={i}
+              className='animate-pulse bg-blue-50 border border-blue-100 h-64 rounded-lg shadow-sm'
+            ></div>
+          ))}
+        </div>
+      </div>
+    )
+  }
+
   return (
     <div className='mt-6'>
       <h2 className='text-2xl font-bold mb-4'>Featured Products</h2>
--- src/app/components/TopProducts/TopProductsContainer.tsx
+++ src/app/components/TopProducts/TopProductsContainer.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import { Product } from '@/app/lib/type'
+import { useEffect, useState } from 'react'
 import 'swiper/css'
 import 'swiper/css/navigation'
 import 'swiper/css/pagination'
@@ -13,10 +14,25 @@ type TopProductsContainerProps = {
 }
 
 export default function TopProductsContainer({ products }: TopProductsContainerProps) {
+  const [isClient, setIsClient] = useState(false)
+
+  useEffect(() => {
+    setIsClient(true)
+  }, [])
+
+  if (!isClient) {
+    return (
+      <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 mt-6'>
+        {products.slice(0, 5).map((p) => (
+          <div key={p.id} className='animate-pulse bg-gray-100 h-64 rounded-lg'></div>
+        ))}
+      </div>
+    )
+  }
+
   return (
     <div className='mt-6'>
       <h2 className='text-2xl font-bold mb-4'>Top Products</h2>
-
       <Swiper
         modules={[Navigation]}
         spaceBetween={20}
