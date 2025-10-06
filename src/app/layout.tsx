9b73f36ed2ef90da318621115e4fa5fe15c1da92
shariar-duke <shariarmahmud225@gmail.com>
2025-10-06 22:38:28 +0600

    fix : background changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,7 +30,7 @@ export default function RootLayout({
   return (
     <html lang='en'>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased flex flex-col min-h-screen`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased flex flex-col min-h-screen bg-white text-black`}
       >
         <ReduxProvider>
           <Toaster position='top-center' reverseOrder={false} />
